4f9ce7bd12d51c630a545bd1bd636a20a1b0ffad
jslee <jslee@plaif.com>
2023-01-14 12:40:30 +0900

    GET 방식의 helloAPI 추가

--- myapi/urls.py
+++ myapi/urls.py
@@ -13,9 +13,20 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# from django.contrib import admin
+# from django.urls import path
+
+# # url과 정규표현식으로 설정하는 기존코드는 레거시한 방식이므로
+# # path로 새롭게 작^^성
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+# ]
+
+# ?? 기존이랑 똑같은데... 여튼;;;
+from django.urls import path, include
 from django.contrib import admin
-from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('quiz/', include('quiz.urls')),
 ]
